c7a22c5a4007865cba7eecb9c056d4bd2e4913cc
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-21 21:28:57 -0500

    Edited Hero Image, Corrected Desktop mid-level structure

--- js/index.js
+++ js/index.js
@@ -1,39 +1,24 @@
-class Dropdown {
-    constructor(element) {
-      
-      // Assign this.element to the dropdown element
-      this.element = element;
-      
-      // Get the element with the ".dropdown-button" class found in the dropdown element 
-      // (look at the HTML for context)
-      this.button = this.element.querySelector('.dropdown-button');
-      
-      // assign the reference to the ".dropdown-content" class found in the dropdown element
-      this.content = this.element.querySelector('.dropdown-content');
-      
-      // Add a click handler to the button reference and call the toggleContent method.
-      this.button.addEventListener('click', () => this.toggleContent());
-    }
-  
-    toggleContent() {
-      
-      // Toggle the ".dropdown-hidden" class off and on
-      this.content.classList.toggle('dropdown-hidden');
-    }
-  }
-  
-  
-  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-
-  const cta = document.querySelector(".cta");
-
-  cta.addEventListener('onclick',function(obj){
-    obj.style.cursor = 'grabbing';
-  });
+class ListItem {
+  constructor(item){
+  this.item = item;
+  this.flashBackground = this.item.querySelector('.flash-background');
+  this.normalBackground = this.item.querySelector('.normal-background');
+  };
+};
 
 
-  const menuItems = document.querySelectorAll('.nav-items');
-  navItems.addEventListener('click', function(event){
+const menuItems = document.querySelectorAll('.nav-items');
+menuItems.forEach(function(item){
+  item.addEventListener('click', function(event){
     event.preventDefault();
   });
+});
+
+const featureBlocks = document.querySelectorAll('.list-item');
+featureBlocks.forEach((item) => new ListItem(item));
+
+featureBlocks.forEach(function(item){
+  item.addEventListener('click', function(event){
+    event.style.border = "solid white 5px";
+  });
+})
